0d6069e6b224fda6bb2b5f8e419faec951e99ba1
Basant kumar Hota <basahota@users.noreply.github.com>
2017-11-21 19:57:19 +0530

    #Code

---
+++ src/main/java/com/dev/pradeep/WebAppLogin/WebAppLoginApplication.java
@@ -0,0 +1,12 @@
+package com.dev.pradeep.WebAppLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppLoginApplication.class, args);
+	}
+}
